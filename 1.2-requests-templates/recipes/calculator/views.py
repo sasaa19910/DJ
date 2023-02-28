8a69d43fe151ba55bb256264899a48c67adb1664
Alexander Platonov <sasa20sasha19910@yandex.ru>
2023-02-28 15:52:28 +0300

    add d

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 
 DATA = {
@@ -18,13 +19,24 @@ DATA = {
     },
     # можете добавить свои рецепты ;)
 }
+def recept(request, name_recipe):
+    recipe = name_recipe
+    servings = int(request.GET.get('serving', 1))
+    if servings:
+        for key, values in DATA[f'{recipe}'].items():
+            DATA[f'{recipe}'][key] = values * servings
+        context = {'recipe': DATA[f'{recipe}']
+                   }
+        return render(request, 'calculator/index.html', context)
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+    else:
+        context = {
+            'recipe': DATA[f'{recipe}']
+        }
+    return render(request, 'calculator/index.html', context)
+
+    
+
+
+
+
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from .models import Phone
+
 
 
 def index(request):
@@ -7,11 +9,25 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phones = Phone.objects.all()
+    sort = request.GET.get('sort')
+    if sort == 'name':
+        res = phones.order_by('name')
+    elif sort == 'min_price':
+        res = phones.order_by('price')
+    elif sort == 'max_price':
+        res = phones.order_by('-price')
+    else:
+        res = phones
+
+
+    context = {'phones': res}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    slugs = Phone.objects.get(slug=slug)
+
+    context = {'phone':  slugs}
     return render(request, template, context)
